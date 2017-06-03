56bf237d9b9d521908a33d39538e5c2e500667b3
djekoff <djekoff@gmail.com>
2017-06-04 00:57:40 +0300

    [*] Правка страницы Страница не найдена

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -5,17 +5,11 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import H1 from 'components/H1';
-import messages from './messages';
 
 export default function NotFound() {
   return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
+    <main className="o-container o-container--large u-pillar-box--small">
+      <h2 className="c-heading c-heading--small">Страница не найдена</h2>
+    </main>
   );
 }
